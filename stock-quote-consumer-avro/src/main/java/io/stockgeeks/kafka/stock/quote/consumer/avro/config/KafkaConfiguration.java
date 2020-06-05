161330114046de17c373f0193f4102239fdc0fe8
Tim <tim@jtim.nl>
2020-06-05 18:28:20 +0200

    Introduce Logging Error handler. To log the details of the poison pill.

--- stock-quote-consumer-avro/src/main/java/io/stockgeeks/kafka/stock/quote/consumer/avro/config/KafkaConfiguration.java
+++ stock-quote-consumer-avro/src/main/java/io/stockgeeks/kafka/stock/quote/consumer/avro/config/KafkaConfiguration.java
@@ -1,10 +1,19 @@
 package io.stockgeeks.kafka.stock.quote.consumer.avro.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.listener.LoggingErrorHandler;
 
 @Configuration
 @EnableKafka
 public class KafkaConfiguration {
 
+  /**
+   * Boot will autowire this into the container factory.
+   */
+  @Bean
+  public LoggingErrorHandler errorHandler() {
+    return new LoggingErrorHandler();
+  }
 }
